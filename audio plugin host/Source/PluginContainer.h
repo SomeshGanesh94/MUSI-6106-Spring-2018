ff28a8b35114da8415c72a059e77d9d094946e43
agneyakerure <kerure.agneya@gmail.com>
2018-02-03 14:22:07 -0500

    Parameter manipulation

---
+++ audio plugin host/Source/PluginContainer.h
@@ -0,0 +1,28 @@
+/*
+  ==============================================================================
+
+    PluginContainer.h
+    Created: 3 Feb 2018 11:32:26am
+    Author:  Agneya Kerure
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include "../JuceLibraryCode/JuceHeader.h"
+
+class PluginContainer
+{
+public:
+    PluginContainer();
+    ~PluginContainer();
+    void setPluginInstance (AudioProcessor& pluginInstance);
+    void setProcessor (AudioProcessor& processor);
+    void setParameter (int parameterIndex, float newValue);
+    String getParameterName (int parameterIndex);
+private:
+    AudioProcessor* pluginInstance;
+    AudioProcessor* newProcessor;
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (PluginContainer)
+};
