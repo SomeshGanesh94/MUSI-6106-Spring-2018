aabbd1defdac87a8f89efce8a5374cde5150db0e
agneyakerure <kerure.agneya@gmail.com>
2018-02-09 16:53:45 -0500

    Updated gitignore

--- audio plugin host/Source/HostStartup.cpp
+++ audio plugin host/Source/HostStartup.cpp
@@ -27,6 +27,7 @@
 #include "../JuceLibraryCode/JuceHeader.h"
 #include "MainHostWindow.h"
 #include "InternalFilters.h"
+//#include "../essentia/essentia.h"
 
 #if ! (JUCE_PLUGINHOST_VST || JUCE_PLUGINHOST_VST3 || JUCE_PLUGINHOST_AU)
  #error "If you're building the audio plugin host, you probably want to enable VST and/or AU support"
