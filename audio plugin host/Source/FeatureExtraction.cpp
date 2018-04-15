691d5c44899e46d443a5ab3a5cd0e4deb83de90b
Somesh Ganesh <someshganesh94@gmail.com>
2018-04-14 20:43:41 -0400

    Integrated Essentia and Feature extraction files

---
+++ audio plugin host/Source/FeatureExtraction.cpp
@@ -0,0 +1,228 @@
+/*
+  ==============================================================================
+
+    FeatureExtraction.cpp
+    Created: 14 Apr 2018 8:09:45pm
+    Author:  Somesh Ganesh
+
+  ==============================================================================
+*/
+
+#include "FeatureExtraction.h"
+#include "essentia/essentia.h"
+#include "essentia/algorithmfactory.h"
+#include "essentia/essentiamath.h"
+#include "essentia/pool.h"
+
+using namespace essentia;
+using namespace essentia::standard;
+
+void CFeatureExtraction::setSampleRate(int iParamValue)
+{
+    m_iSampleRate = iParamValue;
+}
+
+void CFeatureExtraction::setFrameSize(int iParamValue)
+{
+    m_iFrameSize = iParamValue;
+}
+
+void CFeatureExtraction::setHopSize(int iParamValue)
+{
+    m_iHopSize = iParamValue;
+}
+
+int CFeatureExtraction::getSampleRate()
+{
+    return m_iSampleRate;
+}
+
+int CFeatureExtraction::getFrameSize()
+{
+    return m_iFrameSize;
+}
+
+int CFeatureExtraction::getHopSize()
+{
+    return m_iHopSize;
+}
+
+void CFeatureExtraction::initEssentia()
+{
+    essentia::init();
+}
+
+void CFeatureExtraction::shutDownEssentia()
+{
+    essentia::shutdown();
+}
+
+void CFeatureExtraction::doFeatureExtract(string sInputFilePath, string sOutputFilePath)
+{
+    // creating algorithms
+    Pool pPool;
+    
+    AlgorithmFactory& factory = standard::AlgorithmFactory::instance();
+    
+    Algorithm* Aaudio = factory.create("MonoLoader", "filename", sInputFilePath, "sampleRate", m_iSampleRate);
+    
+    Algorithm* AframeCutter = factory.create("FrameCutter",
+                                             "frameSize", m_iFrameSize,
+                                             "hopSize", m_iHopSize);
+    
+    Algorithm* Awindow = factory.create("Windowing", "type", "blackmanharris62");
+    
+    Algorithm* Aspectrum = factory.create("Spectrum");
+    
+    Algorithm* Apitch = factory.create("PitchYin", "frameSize", m_iFrameSize, "sampleRate", m_iSampleRate);
+    Algorithm* Amfcc = factory.create("MFCC");
+    Algorithm* Arms = factory.create("RMS");
+    Algorithm* Azcr = factory.create("ZeroCrossingRate");
+    Algorithm* AspecCent = factory.create("SpectralCentroidTime");
+    Algorithm* ArollOff = factory.create("RollOff", "sampleRate", m_iSampleRate);
+    Algorithm* Aflux = factory.create("Flux");
+    Algorithm* AspecPeak = factory.create("SpectralPeaks", "sampleRate", m_iSampleRate, "maxPeaks", 5, "minFrequency", 1);
+    Algorithm* AspecContrast = factory.create("SpectralContrast", "frameSize", m_iFrameSize, "sampleRate", m_iSampleRate);
+    Algorithm* AharmonicAmp = factory.create("HarmonicPeaks", "maxHarmonics", 10);
+    
+    
+    // connecting the algorithms
+    cout << "Connecting the algorithms" << endl;
+    
+    std::vector<Real> vrAudioBuffer;
+    std::vector<Real> vrFrame, vrWindowedFrame;
+    std::vector<Real> vrSpectrum;
+    std::vector<Real> vrSpecPeakAmp, vrSpecPeakFreq, vrSpecContrastCoeff, vrSpecContrastMag, vrMfccBands, vrMfccCoeff, vrHarmonicAmpFreq, vrHarmonicAmpMag;
+    Real rRms, rZcr, rSpecCent, rRollOff, rFlux, rPitch, rPitchConf;
+    
+    // audio->frame cutter
+    Aaudio->output("audio").set(vrAudioBuffer);
+    AframeCutter->input("signal").set(vrAudioBuffer);
+    
+    // frame cutter->windowing->spectrum
+    AframeCutter->output("frame").set(vrFrame);
+    Awindow->input("frame").set(vrFrame);
+    Awindow->output("frame").set(vrWindowedFrame);
+    Aspectrum->input("frame").set(vrWindowedFrame);
+    Aspectrum->output("spectrum").set(vrSpectrum);
+    
+    // features
+    Apitch->input("signal").set(vrAudioBuffer);
+    Apitch->output("pitch").set(rPitch);
+    Apitch->output("pitchConfidence").set(rPitchConf);
+    
+    Arms->input("array").set(vrFrame);
+    Arms->output("rms").set(rRms);
+    
+    Azcr->input("signal").set(vrFrame);
+    Azcr->output("zeroCrossingRate").set(rZcr);
+    
+    AspecCent->input("array").set(vrFrame);
+    AspecCent->output("centroid").set(rSpecCent);
+    
+    ArollOff->input("spectrum").set(vrSpectrum);
+    ArollOff->output("rollOff").set(rRollOff);
+    
+    Aflux->input("spectrum").set(vrSpectrum);
+    Aflux->output("flux").set(rFlux);
+    
+    AspecPeak->input("spectrum").set(vrSpectrum);
+    AspecPeak->output("magnitudes").set(vrSpecPeakAmp);
+    AspecPeak->output("frequencies").set(vrSpecPeakFreq);
+    
+    AspecContrast->input("spectrum").set(vrSpectrum);
+    AspecContrast->output("spectralContrast").set(vrSpecContrastCoeff);
+    AspecContrast->output("spectralValley").set(vrSpecContrastMag);
+    
+    AharmonicAmp->input("frequencies").set(vrSpecPeakFreq);
+    AharmonicAmp->input("magnitudes").set(vrSpecPeakAmp);
+    AharmonicAmp->input("pitch").set(rPitch);
+    AharmonicAmp->output("harmonicFrequencies").set(vrHarmonicAmpFreq);
+    AharmonicAmp->output("harmonicMagnitudes").set(vrHarmonicAmpMag);
+    
+    Amfcc->input("spectrum").set(vrSpectrum);
+    Amfcc->output("bands").set(vrMfccBands);
+    Amfcc->output("mfcc").set(vrMfccCoeff);
+    
+    cout << "Start processing" << endl;
+    
+    Aaudio->compute();
+    Apitch->compute();
+    
+    while(true)
+    {
+        AframeCutter->compute();
+        
+        if(!vrFrame.size())
+        {
+            break;
+        }
+        
+        if(isSilent(vrFrame))
+        {
+            continue;
+        }
+        
+        Awindow->compute();
+        Aspectrum->compute();
+        Arms->compute();
+        Azcr->compute();
+        AspecCent->compute();
+        ArollOff->compute();
+        Aflux->compute();
+        AspecPeak->compute();
+        AspecContrast->compute();
+        AharmonicAmp->compute();
+        Amfcc->compute();
+        
+        pPool.add("rms", rRms);
+        pPool.add("zcr", rZcr);
+        pPool.add("specCent", rSpecCent);
+        pPool.add("rollOff", rRollOff);
+        pPool.add("flux", rFlux);
+        pPool.add("specPeak", vrSpecPeakAmp);
+        pPool.add("specContrast", vrSpecContrastCoeff);
+        pPool.add("harmonicAmpMag", vrHarmonicAmpMag);
+        pPool.add("harmonicAmpFreq", vrHarmonicAmpFreq);
+        pPool.add("mfcc", vrMfccCoeff);
+        
+        cout << "New frame" << endl;
+        //        cout << vrMfccCoeff << endl;
+    }
+    
+    cout << "Aggregating results" << endl;
+    
+    Pool pAggrPool;
+    const char* stats[] = {"mean", "var", "min", "max"};
+    
+    Algorithm* Aaggr = AlgorithmFactory::create("PoolAggregator", "defaultStats", arrayToVector<string>(stats));
+    
+    Aaggr->input("input").set(pPool);
+    Aaggr->output("output").set(pAggrPool);
+    Aaggr->compute();
+    
+    //    cout << pAggrPool.getRealPool() << endl;
+    
+    Algorithm* Aoutput = AlgorithmFactory::create("YamlOutput", "filename", sOutputFilePath);
+    
+    Aoutput->input("pool").set(pAggrPool);
+    Aoutput->compute();
+    
+    cout << "Done" << endl;
+    
+    delete Aaudio;
+    delete AframeCutter;
+    delete Awindow;
+    delete Aspectrum;
+    delete Apitch;
+    delete Amfcc;
+    delete Arms;
+    delete Azcr;
+    delete AspecCent;
+    delete ArollOff;
+    delete Aflux;
+    delete AspecPeak;
+    delete AspecContrast;
+    delete AharmonicAmp;
+    delete Aoutput;
+}
---
+++ audio plugin host/Source/FeatureExtraction.h
@@ -0,0 +1,43 @@
+/*
+  ==============================================================================
+
+    FeatureExtraction.h
+    Created: 14 Apr 2018 8:09:45pm
+    Author:  Somesh Ganesh
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include <stdio.h>
+#include <string>
+
+using namespace std;
+
+class CFeatureExtraction
+{
+public:
+    
+    CFeatureExtraction(){}
+    ~CFeatureExtraction(){}
+    
+    void setSampleRate(int iParamValue);
+    void setFrameSize(int iParamValue);
+    void setHopSize(int iParamValue);
+    
+    int getSampleRate();
+    int getFrameSize();
+    int getHopSize();
+    
+    void initEssentia();
+    void shutDownEssentia();
+    
+    void doFeatureExtract(string sInputFilePath, string sOutputFilePath);
+    
+private:
+    int m_iSampleRate;
+    int m_iFrameSize;
+    int m_iHopSize;
+    
+};
